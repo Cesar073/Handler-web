3066ebcb14f6b3e166d11a62759adab79dedbd9b
Cesar <cesarbaby@gmail.com>
2022-12-11 12:17:08 -0300

    First commit

---
+++ handler_web.py
@@ -0,0 +1,415 @@
+"""
+Resumen:
+El módulo está pensado para ser utilizado en cualquier web.
+Es una clase donde se puede crear una instancia para cada programa ya que puede manipular diversas pestañas si así se requiera.
+Su configuración y uso estará definido mediante un diccionario desde donde obtendremos los pasos que el handler tiene que hacer y las configuraciones necesarias para obtener cada elemento.
+En otras palabras, el programa sólo lee un diccionario de datos y la configuración del mismo es quién se encarga de ejecutarlo todo.
+
+Proceso:
+Cuando se inicia el Handler se le indica por parámetro la configuración general.
+Luego se fijan los elementos recurrentes. Cuando tenemos que realizar una acción hay que indicar qué tipo de elemento es, por tanto, cada uno requiere de una config mínima pero se repite contantemente. Por ello podemos dejar guardado los elementos que sabemos que se puede usar más de una vez, pero no es obligatorio.
+
+Al crear el objeto que va a manejar el programa vamos a necesitar una serie de configuraciones y los pasos a seguir, los diccionarios son:
+-----------------------------------------------------------------------------------------------------------------------------------------
+config:
+select_driver: [] / Edge / Chrome / Firefox / Safari > Si está en None, se intentará buscar en la misma carpeta donde estamos el driver y en ese mismo orden. De lo contrario, esperamos una lista de 2 elementos siendo el 1ro un str (ej: "Edge") y el 2do un path para buscarlo.
+maximize_window: T/F > Maximizar o no la ventana. Por defecto no ocupa toda la pantalla.
+move_mouse: T/F > Indicamos si estamos en presencia de una web que necesita manipular el mouse, en éste caso habría que dar una advertencia.
+
+elements:
+- text: True / False > En muchos casos los elementos se deben seleccionar primero para luego hacer clic, en vez de realizarlo en 2 pasos lo configuramos en éste apartado.
+- action: clic / edit / select > El select es por si sólo debemos seleccionarlo pero no realizar un clic.
+- absolute_path: (str) > 
+- relative_path: (str) >
+
+elements:
+- text: (str) > 
+- type: (str) > Indica el tipo de elemento con el cuál luego vamos a buscarlo con Selenium (NAME, ID, XPATH, etc)
+- value: (str) > Valor con el que vamos a buscar el elemento en función a su type.
+    - parts: (optional | list) > Cuando tenemos un XPATH por ejemplo que queremos recorrer modificando el valor de una parte del mismo, podemos construirlo dinámicamente. Se espera una lista de 3 partes donde la del medio es un valor que será reemplazado por un número determinado a continuación.
+    - 
+
+ejemplo_de_elementos = {
+    1: {
+        "text": False,
+        "type": "NAME",
+        "value": "email",
+    },
+    2: {
+        "text": False,
+        "type": "XPATH",
+        "value": '//*[@id="platform-container"]/div/div/div/div[1]/platform-courses/div/div/div/div/div[2]/div[2]/div[1]/ul/li[1]/a/span',
+    },
+    # Courses - List.div - Encontrar UN elemento dentro de una lista (Entregas)
+    3: {
+        "text": "", # Si buscamos algún texto en el elemento para distinguirlo de la lista
+        "type": "XPATH",
+        "parts": [
+            '//*[@id="platform-container"]/div/div/div/div[1]/platform-course/div/div/div[2]/div/div/div/div/div[2]/div[1]/div[3]/div/div[',
+            '_pos_',
+            ']/a/div/div/div[1]/div/b',
+        ],
+        # Se recorre el XPATH modificando el valor del campo '_pos_', el mismo se coordina en estas keys (start y end)
+        "start": 1,
+        "end": 20,
+        "end_loop": True,
+    },
+}
+
+
+Modo de uso:
+-----------
+Haciendo uso de sus métodos (run_steps y run_tasks), podemos correr pasos o tareas.
+TASKS: Son acciones mínimas encadenadas como pueden ser seleccionar un EDIT y escribir algo. Pueden ser cortas o extensas y se ejecutan en orden.
+STEPS: Si necesitamos algo más complejo podemos llamar a los pasos o steps, que se encargan de ejecutar muchas tareas en un bucle pudiendo repetirse las mismas en base a una condición o resultado determinado.
+
+steps:
+El método run_steps 
+
+tasks:
+{
+    "open_url":{
+        "url": "www.google.com",
+        "new_window": False,
+        "change_tab": False,
+        },
+    "input_text":{
+        "
+    }
+
+#########################################################################################################################################
+ACA PONEMOS LO QUE SABEMOS QUE FALTA INCORPORAR, PERO PRIMERO VAMOS A ENFOCARNOS EN QUE FUNCIONE:
+AGREGAR:
+(01) - En el init, habría que crear una excepción para cuando no se pudo obtener el driver.
+(02)
+(03) - Cuando sepa como abrir una web en otra pestaña, es decir, sin cerrar la pestaña actual.
+(04) - Ahora sabemos que todo lo que buscamos lo encontramos, pero tenemos que poner un try porque si cambia la web eso pincha, hay que prevenirlo
+(XX) - Cambiar todos los prints y explotar al máximo la librería de logging.
+(XX) - Para el parámetro de "parts" se construye en base a un lista se crea con valores enteros no pudiendo usar otra opción. Debería ser una lista de valores entonces si fuesen enteros se los puede crear con un range desde la app que lo ejecuta en vez de forzar a ser números desde acá.
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+import threading
+
+import pyautogui as pa
+
+
+class HandlerWebSelenium:
+
+    def __init__(self, config: dict, elements: list):
+        """
+        Args:
+            config: Recibimos un dict con la info que necesitamos para todo el proceso.
+            elements: Forma de identificar y seleccionar cualquier elemento en la web.
+        """
+
+        # (01)
+        if config["select_driver"]:
+            path = config["select_driver"][1]
+            if config["select_driver"][0] == "Edge":
+                self.driver = webdriver.Edge(path)
+            elif config["select_driver"][0] == "Chrome":
+                self.driver = webdriver.Chrome(path)
+            elif config["select_driver"][0] == "Firefox":
+                self.driver = webdriver.Firefox(path)
+            elif config["select_driver"][0] == "Safari":
+                self.driver = webdriver.Safari(path)
+        else:
+            # (02) Programar una forma de intentar buscar todos los posibles drivers en la carpeta raíz.
+            pass
+
+        if config["maximize_window"]:
+                self.driver.maximize_window()
+        self.move_mouse = config["move_mouse"]
+        self.elements = elements
+        self.activity_mouse = False
+        self.q_tab = -1
+
+    def run_steps(self, value_return, steps: dict, tasks: dict):
+        """
+        Método que se encarga de ir leyendo las tareas que se tienen que realizar.
+        Las mismas deben tener un metodo que indique cuándo cortar el bucle si se genera uno infinito.
+        """
+        self.steps = steps
+        self.tasks = tasks
+        print("**** INICIO STEP ****")
+        step_counter = 0
+        self.list_of_list = []
+        
+        while True:
+            step_counter += 1
+            if not self.steps.get(step_counter, 0):
+                print(f"**** END LOOP ****\n")
+                break
+            print(f"Name step: {self.steps[step_counter]['name_step']}")
+            if self.steps[step_counter].get("stop", False):
+                break
+            value = self.run_tasks(self.tasks[self.steps[step_counter]["name_step"]])
+
+            if self.steps[step_counter]["is_false_finish"]:
+                if value == False:
+                    break
+
+            if self.steps[step_counter]["is_true_continue"]:
+                if value:
+                    pass
+                else:
+                    value -= 1
+        
+        # Retornamos la lista que se haya generado
+        if value_return == "list":
+            return self.list_of_list
+
+    def run_tasks(self, task, element = ""):
+        """
+        Le llegan tareas, cada una se ejecuta en orden en función a su nombre.
+        """
+        
+        print(f"Name task: {task.get('name_task', 'N/N')}")
+        try:
+            _lenght = len(task)
+            counter = 0
+            while counter < _lenght:
+                for keys, values in task[counter].items():
+
+                    if keys == "name_task":
+                        continue
+
+                    if keys == "open_url":
+                        self.q_tab += 1
+                        if values["new_tab"]:
+                            self.driver.execute_script("window.open('');")
+                            sleep(0.5)
+                            self.driver.switch_to.window(self.driver.window_handles[self.q_tab])
+                            sleep(0.5)
+                        if self.move_mouse:
+                            if values["move_mouse"]:
+                                threading.Thread(target=self._move_mouse).start()
+                        self.driver.get(values["url"])
+                        self.driver.implicitly_wait(values.get("implicitly_wait", 50))
+                    
+                    elif keys == "find_element":
+                        element = self._find_element(values["pos_element"], bucle=values.get("implicitly_wait", 50))
+
+                    elif keys == "find_elements_for_text":
+                        _element = self.elements[values["pos_element"]]
+                        for pos in range(_element["start"], _element["end"]):
+                            try:
+                                path = ""
+                                for text in _element["parts"]:
+                                    if text == "_pos_":
+                                        path += str(pos)
+                                    else:
+                                        path += text
+
+                                new_dict = {
+                                    "type": _element["type"],
+                                    "value": path,
+                                }
+                                element = self._find_element(False,new_dict)
+                                
+                                if element.text == _element["text"]:
+                                    if values["click"]:
+                                        element.click()
+                                        break
+                            except Exception:
+                                if values["end_loop"]:
+                                    break
+
+                    elif keys == "write":
+                        element.send_keys(values["text"])
+
+                    elif keys == "click":
+                        if values:
+                            element = self._find_element(values["pos_element"],values.get("implicitly_wait", 50))
+                        element.click()
+
+                    elif keys == "loop_list":
+                        for pos in range(values["start"], values["end"] + 1):
+                            try:
+                                path = ""
+                                for text in values["parts"]:
+                                    if text == "_pos_":
+                                        path += str(pos)
+                                    else:
+                                        path += text
+
+                                new_dict = {
+                                    "type": values["type"],
+                                    "value": path,
+                                }
+                                element = self._find_element(False,new_dict)
+                                if values["text"]:
+                                    self.list_of_list.append(element.text)
+                                else:
+                                    self.list_of_list.append(element)
+                            except Exception:
+                                if values["end_loop"]:
+                                    break
+                        print(f"\nContenido de la lista obtenida:\nContenido:\n{self.list_of_list}\n\n")
+                    
+                    if values.get("sleep", 0):
+                        self.time_sleep(values['sleep'], values.get('type_sleep', "simple_show"))
+                counter += 1
+
+        except Exception as e:
+            print(f"An error has occurred: {e}")
+            return False
+        return True
+
+    def _find_element(self, value, new_dict = {}, bucle = 0):
+        # (04)
+        if new_dict:
+            _type = new_dict["type"]
+            _value = new_dict["value"]
+        else:
+            _type = self.elements[value]["type"]
+            _value = self.elements[value]["value"]
+
+        while True:
+            if _type == "NAME":
+                By_VALUE = By.NAME
+            elif _type == "CLASS_NAME":
+                By_VALUE = By.CLASS_NAME
+            elif _type == "CSS_SELECTOR":
+                By_VALUE = By.CSS_SELECTOR
+            elif _type == "ID":
+                By_VALUE = By.ID
+            elif _type == "XPATH":
+                By_VALUE = By.XPATH
+            elif _type == "LINK_TEXT":
+                By_VALUE = By.LINK_TEXT
+            elif _type == "PARTIAL_LINK_TEXT":
+                By_VALUE = By.PARTIAL_LINK_TEXT
+            
+            try:
+                element = self.driver.find_element(by=By_VALUE, value=_value)
+                if element:
+                    return element
+            except NoSuchElementException:
+                print(f"No se encontró el elemento")
+                if not bucle:
+                    return None
+                elif bucle == -1:
+                    pass
+                elif bucle > 0:
+                    bucle -= 1
+                else:
+                    return element
+
+        
+        # Devolvemos el elemento
+        return element
+
+    def _move_mouse(self):
+        """La función se ejecuta mientras la variable move_mouse sea True, por lo que se puede desactivar desde fuera.
+        Pero genera 8 bucles recorriendo varias partes de la pantalla y una vez realizado termina sóla.
+        Cada bucle contiene 20 iteracciones."""
+        
+        self.activity_mouse = True
+        # Posición inferior izquierda
+        eje_x = 0
+        eje_y = 0
+
+        # Bucle 1: Horizontal Central
+        x = eje_x + 50
+        y = eje_x + 540
+        counter = 0
+
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            x += 100
+            counter += 1
+        
+        # Bucle 2: Horizontal Superior
+        x = eje_x + 50
+        y = eje_y + 50
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            x += 720
+            counter += 1
+        
+        # Bucle 3: Horizontal Inferior
+        x = eje_x + 50
+        y = eje_y + 360
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            x += 100
+            counter += 1
+        
+        # Bucle 4: Diagonal (sup - inf)
+        x = eje_x + 50
+        y = eje_y + 1050
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            x += 100
+            y -= 50
+            counter += 1
+        
+        # Bucle 5: Vertical Central
+        x = eje_x + 960
+        y = eje_y + 1050
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            y -= 50
+            counter += 1
+        
+        # Bucle 6: Diagonal (inf - sup)
+        x = eje_x + 50
+        y = eje_y + 50
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            x += 100
+            y += 50
+            counter += 1
+        
+        # Bucle 7: Vertical Izquierdo
+        x = eje_x + 480
+        y = eje_y + 1050
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            y -= 50
+            counter += 1
+        
+        # Bucle 8: Vertical Derecho
+        x = eje_x + 1440
+        y = eje_y + 1050
+        counter = 0
+        while counter < 20 and self.activity_mouse:
+            pa.moveTo(x=x, y=y)
+            sleep(0.1)
+            y -= 50
+            counter += 1
+        
+        self.move_mouse = False
+
+    def time_sleep(self, time, type_sleep):
+        if type_sleep == "simple_show":
+            print(f"Sleep: {time}")
+            sleep(time)
+        elif type_sleep == "seconds_show":
+            while time > 0:
+                if time > 1:
+                    print(f"Sleep: {time}")
+                    sleep(1)
+                time -= 1
+            print(f"Sleep: 0")
+        else:
+            sleep(time)
+
